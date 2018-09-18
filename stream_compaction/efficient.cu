73e1d5f7eb5c8a89332762ceb675eb057e6d1b82
Ishan Ranade <ranadeishan@gmail.com>
2018-09-18 16:11:54 -0400

    got efficient power of two

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -2,6 +2,8 @@
 #include <cuda_runtime.h>
 #include "common.h"
 #include "efficient.h"
+#include <device_launch_parameters.h>
+
 
 namespace StreamCompaction {
     namespace Efficient {
@@ -12,13 +14,69 @@ namespace StreamCompaction {
             return timer;
         }
 
+        __global__ void kernUpsweep(int n, int power, int *array) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+
+            if (index < n) {
+                for (int k = 0; k < n; k += 2 * power) {
+                    array[k + (2 * power) - 1] = array[k + power - 1] + array[k + (2 * power) - 1];
+                }
+            }
+        }
+
+        __global__ void kernDownsweep(int n, int power, int *array) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+
+            if (index < n) {
+                for (int k = 0; k < n; k += 2 * power) {
+                    int t = array[k + power - 1];
+                    array[k + power - 1] = array[k + (2 * power) - 1];
+                    array[k + (2 * power) - 1] = t + array[k + (2 * power) - 1];
+                }
+            }
+        }
+
+        __global__ void kernSetZero(int n, int *array) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+
+            if (index < n) {
+                if (index == n - 1) {
+                    array[index] = 0;
+                }
+            }
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            int *temp;
+            cudaMalloc((void**)&temp, n * sizeof(int));
+            cudaMemcpy(temp, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            cudaDeviceSynchronize();
+
             timer().startGpuTimer();
+
+            int blockSize = 256;
+            int blocks = (n + blockSize - 1) / blockSize;
+
             // TODO
+            for (int d = 0; d < ilog2ceil(n); ++d) {
+                kernUpsweep << <blocks, blockSize >> > (n, pow(2, d), temp);
+                cudaDeviceSynchronize();
+            }
+            
+            kernSetZero << <blocks, blockSize >> > (n, temp);
+
+            for (int d = ilog2ceil(n) - 1; d >= 0; --d) {
+                kernDownsweep << <blocks, blockSize >> > (n, pow(2, d), temp);
+                cudaDeviceSynchronize();
+            }
+
             timer().endGpuTimer();
+
+            cudaMemcpy(odata, temp, n * sizeof(int), cudaMemcpyDeviceToHost);
         }
 
         /**
@@ -32,7 +90,9 @@ namespace StreamCompaction {
          */
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
+            
             // TODO
+
             timer().endGpuTimer();
             return -1;
         }
